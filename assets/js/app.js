138442ca132f62e0fa4721c59a844f024101eaf5
J.S <jimmy.sene@gmail.com>
2022-06-21 12:24:15 +0200

    step 5 done

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,11 +18,27 @@ var app = {
     }
     // ajouter un écouteur d'event submit sur le formulaire d'ajout de liste
     document.querySelector('#addListModal form').addEventListener('submit', app.handleAddListForm);
-
+    // ajouter un écouteur d'event click sur les boutons +
+    const btnsAddCard = document.querySelectorAll('.panel a.is-pulled-right');
+    for(const btnAddCard of btnsAddCard) {
+      btnAddCard.addEventListener('click', app.showAddCardModal);
+    }
+    // ajouter un écouteur d'évènement submit sur le formulaire d'ajout de carte
+    document.querySelector('#addCardModal form').addEventListener('submit', app.handleAddCardForm);
   },
   showAddListModal: function() {
     // afficher la modale à l'écran
     document.getElementById('addListModal').classList.add('is-active');
+  },
+  showAddCardModal: function(event) {
+    // récupérer l'id de la bonne liste
+    // const idList = event.target.closest('.panel').getAttribute('data-list-id');
+    // vu qu'on récupère la valeur d'un data-attribute (attribut HTML personnalisé) on peut utiliser une autre façon de faire :
+    const idList = event.target.closest('.panel').dataset.listId;
+    // mettre à jour l'input hidden du formulaire de la modale
+    document.querySelector('#addCardModal input[name="list_id"]').value = idList;
+    // afficher la modale de carte
+    document.getElementById('addCardModal').classList.add('is-active');
   },
   hideModals: function() {
     // cacher les modales
@@ -38,10 +54,11 @@ var app = {
     // récupérer les infos du formulaire
     const formData = new FormData(event.target);
     // faire apparaitre une nouvelle liste dans le DOM
-    app.makeListInDOM(formData)
+    app.makeListInDOM(formData);
+    // reset les champs du formulaire
+    event.target.reset();
     // cacher la modale
     app.hideModals();
-    
   },
   makeListInDOM: function(formData) {
     // créer une nouvelle liste dans le DOM
@@ -53,6 +70,30 @@ var app = {
     cloneTemplate.querySelector('h2').textContent = formData.get('name');
     // insérer dans la page concrètement
     document.querySelector('.card-lists').appendChild(cloneTemplate);
+  },
+  handleAddCardForm: function(event) {
+    // empêcher le rechargement de la page
+    event.preventDefault();
+    // récupérer les infos du formulaire
+    const formData = new FormData(event.target);
+    // créer la carte dans le DOM
+    app.makeCardInDOM(formData);
+    // reset les champs du formulaire
+    event.target.reset();
+    // cacher la modale
+    app.hideModals();
+  },
+  makeCardInDOM: function(formData) {
+    // récupérer le template de card
+    const template = document.getElementById('templateCard');
+    // cloner le template
+    const cloneTemplate = document.importNode(template.content, true);
+    // modifier le titre de la carte
+    cloneTemplate.querySelector('.column').textContent = formData.get('name');
+    // insérer le clone du template dans la bonne liste du DOM
+    const listDOM = document.querySelector(`.panel[data-list-id="${formData.get('list_id')}"]`);
+    // on insère la carte dans le block container de list
+    listDOM.querySelector('.panel-block').appendChild(cloneTemplate);
   }
 
 };

7bf5c358e592762eadf5ee99ddeb476dcb94d755
J.S <jimmy.sene@gmail.com>
2022-06-21 10:11:00 +0200

    step 2 done

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,10 +9,26 @@ var app = {
   addListenerToActions: function() {
     // mettre un écouteur d'évènement clic sur le bouton d'ajout de liste
     document.getElementById('addListButton').addEventListener('click', app.showAddListModal);
+    // ajouter un écouteur d'évènement click sur les boutons de fermeture de modale
+    const btnsCloseModal = document.getElementsByClassName('close');
+    // const btnsCloseModal = document.querySelectorAll('.close');
+    for(const btnClose of btnsCloseModal) {
+      // pour chaque bouton close on déclencher la fonction hideModals
+      btnClose.addEventListener('click', app.hideModals);
+    }
+
   },
   showAddListModal: function() {
     // afficher la modale à l'écran
     document.getElementById('addListModal').classList.add('is-active');
+  },
+  hideModals: function() {
+    // cacher les modales
+    const modals = document.getElementsByClassName('modal');
+    for(const modal of modals) {
+      // on cache toutes les modals
+      modal.classList.remove('is-active');
+    }
   }
 
 };

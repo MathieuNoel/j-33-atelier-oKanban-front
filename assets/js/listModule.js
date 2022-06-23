ccad0f43f51f05d55e11586c46f1b6f891e1c7ce
J.S <jimmy.sene@gmail.com>
2022-06-23 09:59:05 +0200

    edit list done

--- assets/js/listModule.js
+++ assets/js/listModule.js
@@ -35,11 +35,52 @@ const listModule = {
         const cloneTemplate = document.importNode(template.content, true);
         // modifier son data attribute id
         cloneTemplate.querySelector('.panel').dataset.listId = list.id;
+        // modifier l'id du input hidden du formulaire d'édition
+        const editForm = cloneTemplate.querySelector('.edit-list-form');
+        editForm.querySelector('input[name="list-id"]').value = list.id;
         // le modifier (nom de la liste)
-        cloneTemplate.querySelector('h2').textContent = list.name;
+        const h2 =  cloneTemplate.querySelector('h2');
+        h2.textContent = list.name;
+        // ajouter un écouteur d'event sur le titre de liste pour afficher le formulaire d'édition
+        h2.addEventListener('dblclick', listModule.showEditListForm);
+        // ajouter un écouteur d'event sur le formulaire d'édition pour pouvoir modifier le nom de la liste
+        editForm.addEventListener('submit', listModule.handleEditListForm);
         // ajouter un écouteur d'event sur le bouton + pour afficher la modale de carte
         cloneTemplate.querySelector('.panel a.is-pulled-right').addEventListener('click', cardModule.showAddCardModal);
         // insérer dans la page concrètement
         document.querySelector('.card-lists').appendChild(cloneTemplate);
       },
+      showEditListForm: function(event) {
+        // cacher le h2
+        event.target.classList.add('is-hidden');
+        // afficher le formulaire d'édition
+        event.target.nextElementSibling.classList.remove('is-hidden');
+      },
+      handleEditListForm: async function(event) {
+        // empêcher le rechargement de la page
+        event.preventDefault();
+        // récupérer les infos du formulaire
+        const formData = new FormData(event.target);
+        const h2 = event.target.previousElementSibling;
+        // faire un call API en PATCH sur /lists/:id
+        try {
+          const response = await fetch(`${utilsModule.base_url}/lists/${formData.get('list-id')}`, {
+            method: 'PATCH',
+            body: formData
+          });
+          // ici on récupère la data (la liste modifiée ou l'erreur)
+          const json = await response.json();
+          // si on a un code d'erreur on renvoie dans le catch
+          if(!response.ok) throw json;
+          // modifie le h2 dans le DOM (eh oui ça ne se fait pas tout seul !)
+          h2.textContent = json.name;
+        } catch(error) {
+          alert('Impossible de modifier la liste !');
+          console.error(error);
+        }
+        // on réaffiche le titre
+        h2.classList.remove('is-hidden');
+        // puis on masque le formulaire
+        event.target.classList.add('is-hidden');
+      }
 }

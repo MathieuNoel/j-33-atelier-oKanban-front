50c10c73f297393b795dfc9d623fa0f1901128af
Mathieu Noel <yzorien@gmail.com>
2022-06-24 14:35:48 +0200

    correction du challenge

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,10 @@
 
 // on objet qui contient des fonctions
 var app = {
-  
   // fonction d'initialisation, lancée au chargement de la page
   init: function () {
     app.getListsFromAPI();
     app.addListenerToActions();
-
-
-      const listsContainer=document.querySelector('.lists-container');
-      // console.log('LALALALA',listsContainer)
-      //!possibilité de déplacer les listes
-      Sortable.create(listsContainer,{
-        onEnd:listModule.handleListMoves
-      });
-    
-
-
   },
   addListenerToActions: function() {
     // mettre un écouteur d'évènement clic sur le bouton d'ajout de liste
@@ -37,6 +25,8 @@ var app = {
     // }
     // ajouter un écouteur d'évènement submit sur le formulaire d'ajout de carte
     document.querySelector('#addCardModal form').addEventListener('submit', cardModule.handleAddCardForm);
+    // on va attacher un écouteur d'event submit sur le formulaire d'association d'un tag sur une carte
+    document.querySelector('#addTagToCardModal form').addEventListener('submit', labelModule.handleAssociateLabelForm);
   },
   // récupérer les listes depuis l'API et les afficher dans le DOM
   getListsFromAPI: async function() {
@@ -46,25 +36,34 @@ var app = {
       // on appelle la méthode json sur l'objet response obtenu pour récupérer directement notre tableau de listes (la data)
       const lists = await response.json();
 
-      
+      console.log(lists);
       
       // dessiner les listes dans le DOM
       for(const list of lists) {
         listModule.makeListInDOM(list);
         for(const card of list.cards) {
           cardModule.makeCardInDOM(card);
+          for(const label of card.labels) {
+            // dessiner les tag dans la carte du DOM
+            labelModule.makeLabelInDOM(label);
+          }
         }
       }
 
+      // Drag & drop des listes
+      // on récupère le container de nos listes, à savoir card-lists
+      const listContainer = document.querySelector('.card-lists');
+      new Sortable(listContainer, {
+        // vu qu'il n'y a qu'un seul container pas besoin de group
+        // on renseigne directement les éléments déplacable dans draggable, ici nos listes (.panel)
+        draggable: '.panel',
+        onEnd: listModule.dragList
+      });
     } catch(error) {
       console.error(error);
       alert('Impossible de récupérer les listes !');
     }
-  },
-
-  
-} 
+  }
+};
 // on accroche un écouteur d'évènement sur le document : quand le chargement est terminé, on lance app.init
 document.addEventListener('DOMContentLoaded', app.init );
-
-
--- assets/js/listModule.js
+++ assets/js/listModule.js
@@ -1,148 +1,130 @@
 const listModule = {
-    showAddListModal: function() {
-        // afficher la modale à l'écran
-        document.getElementById('addListModal').classList.add('is-active');
-    },
-    handleAddListForm: async function(event) {
-        // il faut empêcher le comportement par défaut de l'event submit, à savoir l'envoi d'une requête HTTP et donc le rechargement de la page
-        event.preventDefault();
-        //je met en place un compteur pour donner une position à la liste
-        const position = document.querySelectorAll('.column.is-one-quarter.panel').length 
-        console.log(position);       
-        // récupérer les infos du formulaire
-        const formData = new FormData(event.target);
-        // j'incrémente position contenu dans formData
-        formData.append("position", position+1)
-        try {
-          // on fait un call API en POST sur /lists
-          const response = await fetch(`${utilsModule.base_url}/lists`, {
-            method: 'POST',
-            body: formData
-          });
-          const newList = await response.json();
-          // faire apparaitre une nouvelle liste dans le DOM
-          listModule.makeListInDOM(newList);
-        } catch(error) {
-          console.error(error);
-          alert('Impossible d\'ajouter une liste !');
-        }
-        
-        // reset les champs du formulaire
-        event.target.reset();
-        // cacher la modale
-        utilsModule.hideModals();
-      },
-      makeListInDOM: function(list) {
-        // console.log(list);
-        // créer une nouvelle liste dans le DOM
-        // récupérer le template
-        const template = document.getElementById('templateList');
-        // en faire un clone
-        const cloneTemplate = document.importNode(template.content, true);
-        // modifier son data attribute id
-        cloneTemplate.querySelector('.panel').dataset.listId = list.id;
-        const cardsContainer = cloneTemplate.querySelector('.cards-Container');
-        Sortable.create(cardsContainer, { group:"cartes",
-          onEnd: cardModule.handleCardMoves
-        });
-        // modifier son data attribute position
-        cloneTemplate.querySelector('.panel').dataset.listPosition = list.position;
-        // modifier l'id du input hidden du formulaire d'édition
-        const editForm = cloneTemplate.querySelector('.edit-list-form');
-        editForm.querySelector('input[name="list-id"]').value = list.id;
-        // le modifier (nom de la liste)
-        const h2 =  cloneTemplate.querySelector('h2');
-        h2.textContent = list.name;
-        // ajouter un écouteur d'event sur le titre de liste pour afficher le formulaire d'édition
-        h2.addEventListener('dblclick', listModule.showEditListForm);
-        // ajouter un écouteur d'event sur le formulaire d'édition pour pouvoir modifier le nom de la liste
-        editForm.addEventListener('submit', listModule.handleEditListForm);
-        // ajouter un écouteur d'event sur le bouton + pour afficher la modale de carte
-        cloneTemplate.querySelector('.add-list-icon').addEventListener('click', cardModule.showAddCardModal);
-        // ajouter un écouteur d'event sur le bouton poubelle pour supprimer la liste
-        cloneTemplate.querySelector('.delete-list-icon').addEventListener('click', listModule.deleteList);
-        // insérer dans la page concrètement
-        document.querySelector('.card-lists').appendChild(cloneTemplate);
-        const cardContainer = document.querySelector('.card-lists')
-        new Sortable.create(cardContainer);
-        
-      },
-      showEditListForm: function(event) {
-        // cacher le h2
-        event.target.classList.add('is-hidden');
-        // afficher le formulaire d'édition
-        event.target.nextElementSibling.classList.remove('is-hidden');
-      },
-      handleEditListForm: async function(event) {
-        // empêcher le rechargement de la page
-        event.preventDefault();
-        // récupérer les infos du formulaire
-        const formData = new FormData(event.target);
-        const h2 = event.target.previousElementSibling;
-        // faire un call API en PATCH sur /lists/:id
-        try {
-          const response = await fetch(`${utilsModule.base_url}/lists/${formData.get('list-id')}`, {
-            method: 'PATCH',
-            body: formData
-          });
-          // ici on récupère la data (la liste modifiée ou l'erreur)
-          const json = await response.json();
-          // si on a un code d'erreur on renvoie dans le catch
-          if(!response.ok) throw json;
-          // modifie le h2 dans le DOM (eh oui ça ne se fait pas tout seul !)
-          h2.textContent = json.name;
-        } catch(error) {
-          alert('Impossible de modifier la liste !');
-          console.error(error);
-        }
-        // on réaffiche le titre
-        h2.classList.remove('is-hidden');
-        // puis on masque le formulaire
-        event.target.classList.add('is-hidden');
-      },
-      deleteList: async function(event) {
-        // si l'utilisateur refuse de supprimer la liste sort de la fonction
-        if(!confirm('Voulez-vous vraiment supprimer cette liste ?')) return;
-        // récupérer la liste dans le DOM
-        const listDOM = event.target.closest('.panel');
-        // faire un call API en DELETE sur /lists/:id
-        try {
-          const response = await fetch(`${utilsModule.base_url}/lists/${listDOM.dataset.listId}`, {
-            method: 'DELETE'
-          });
-          const json = await response.json();
-          if(!response.ok) throw json;
-          // supprimer la liste dans le DOM
-          listDOM.remove();
-        } catch(error) {
-          alert('Impossible de supprimer la liste !');
-          console.error(error);
-        }
-      },
-
-      
-    async handleListMoves(e) {
-    const containerElm = e.target;
-    const listsElms = containerElm.children
-    for (let i = 0; i < listsElms.length; i++) {
-      const element = listsElms[i];
-      const formData = new FormData();
-      formData.set("position", i);
-
-      console.log(Object.fromEntries(formData));
+  showAddListModal: function() {
+      // afficher la modale à l'écran
+      document.getElementById('addListModal').classList.add('is-active');
+  },
+  handleAddListForm: async function(event) {
+      // il faut empêcher le comportement par défaut de l'event submit, à savoir l'envoi d'une requête HTTP et donc le rechargement de la page
+      event.preventDefault();
+      // récupérer les infos du formulaire
+      const formData = new FormData(event.target);
       try {
-        const response = await fetch(`${utilsModule.base_url}/lists/${element.dataset.listId}`, {
+        // on fait un call API en POST sur /lists
+        const response = await fetch(`${utilsModule.base_url}/lists`, {
+          method: 'POST',
+          body: formData
+        });
+        const newList = await response.json();
+        // faire apparaitre une nouvelle liste dans le DOM
+        listModule.makeListInDOM(newList);
+      } catch(error) {
+        console.error(error);
+        alert('Impossible d\'ajouter une liste !');
+      }
+      
+      // reset les champs du formulaire
+      event.target.reset();
+      // cacher la modale
+      utilsModule.hideModals();
+    },
+    makeListInDOM: function(list) {
+      // créer une nouvelle liste dans le DOM
+      // récupérer le template
+      const template = document.getElementById('templateList');
+      // en faire un clone
+      const cloneTemplate = document.importNode(template.content, true);
+      // modifier son data attribute id
+      cloneTemplate.querySelector('.panel').dataset.listId = list.id;
+      // modifier l'id du input hidden du formulaire d'édition
+      const editForm = cloneTemplate.querySelector('.edit-list-form');
+      editForm.querySelector('input[name="list-id"]').value = list.id;
+      // le modifier (nom de la liste)
+      const h2 =  cloneTemplate.querySelector('h2');
+      h2.textContent = list.name;
+      // ajouter un écouteur d'event sur le titre de liste pour afficher le formulaire d'édition
+      h2.addEventListener('dblclick', listModule.showEditListForm);
+      // ajouter un écouteur d'event sur le formulaire d'édition pour pouvoir modifier le nom de la liste
+      editForm.addEventListener('submit', listModule.handleEditListForm);
+      // ajouter un écouteur d'event sur le bouton + pour afficher la modale de carte
+      cloneTemplate.querySelector('.add-list-icon').addEventListener('click', cardModule.showAddCardModal);
+      // ajouter un écouteur d'event sur le bouton poubelle pour supprimer la liste
+      cloneTemplate.querySelector('.delete-list-icon').addEventListener('click', listModule.deleteList);
+      // Drag & Drop des cartes au sein de la liste
+      // on va chopper l'élément qui va contenir nos cartes, ici la div panel-block
+      const cardContainer = cloneTemplate.querySelector('.panel-block');
+      new Sortable(cardContainer, {
+        group: 'list',
+        draggable: '.box', // on lui renseigne ce qu'on veut déplacer, à savoir les cartes
+        onEnd: cardModule.dragCard
+      });
+      // insérer dans la page concrètement
+      document.querySelector('.card-lists').appendChild(cloneTemplate);
+    },
+    showEditListForm: function(event) {
+      // cacher le h2
+      event.target.classList.add('is-hidden');
+      // afficher le formulaire d'édition
+      event.target.nextElementSibling.classList.remove('is-hidden');
+    },
+    handleEditListForm: async function(event) {
+      // empêcher le rechargement de la page
+      event.preventDefault();
+      // récupérer les infos du formulaire
+      const formData = new FormData(event.target);
+      const h2 = event.target.previousElementSibling;
+      // faire un call API en PATCH sur /lists/:id
+      try {
+        const response = await fetch(`${utilsModule.base_url}/lists/${formData.get('list-id')}`, {
           method: 'PATCH',
           body: formData
         });
-        if (!response.ok) throw new Error(response.status)
-      } catch (error) {
-        console.log(error);
-        alert(error);
+        // ici on récupère la data (la liste modifiée ou l'erreur)
+        const json = await response.json();
+        // si on a un code d'erreur on renvoie dans le catch
+        if(!response.ok) throw json;
+        // modifie le h2 dans le DOM (eh oui ça ne se fait pas tout seul !)
+        h2.textContent = json.name;
+      } catch(error) {
+        alert('Impossible de modifier la liste !');
+        console.error(error);
       }
-
+      // on réaffiche le titre
+      h2.classList.remove('is-hidden');
+      // puis on masque le formulaire
+      event.target.classList.add('is-hidden');
+    },
+    deleteList: async function(event) {
+      // si l'utilisateur refuse de supprimer la liste sort de la fonction
+      if(!confirm('Voulez-vous vraiment supprimer cette liste ?')) return;
+      // récupérer la liste dans le DOM
+      const listDOM = event.target.closest('.panel');
+      // faire un call API en DELETE sur /lists/:id
+      try {
+        const response = await fetch(`${utilsModule.base_url}/lists/${listDOM.dataset.listId}`, {
+          method: 'DELETE'
+        });
+        const json = await response.json();
+        if(!response.ok) throw json;
+        // supprimer la liste dans le DOM
+        listDOM.remove();
+      } catch(error) {
+        alert('Impossible de supprimer la liste !');
+        console.error(error);
+      }
+    },
+    dragList: function(event) {
+      // récupérer les listes dans un tableau
+      const listsDOM = document.querySelectorAll('.panel');
+      listsDOM.forEach(async (listDOM, index) => {
+        // on créé un formData
+        const formData = new FormData();
+        // et on le rempli avec la position de la liste
+        formData.set('position', index);
+        // faire un call API en PATCH sur /lists/:id
+        const response = await fetch(`${utilsModule.base_url}/lists/${listDOM.dataset.listId}`, {
+          method: 'PATCH',
+          body: formData
+        });
+      });
     }
-  }
 }
-
-
